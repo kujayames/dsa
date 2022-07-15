877d07edcb709d91837a91b0e701870f6391970c
unknown <kujayames@gmail.com>
2022-07-15 05:19:53 -0700

    Flesh out logistic regression more

--- logistic_regression.cpp
+++ logistic_regression.cpp
@@ -1,33 +1,48 @@
+// Trains and uses a logistic classifier.
+#include <cmath>
+#include <exception>
 #include <iostream>
+#include <vector>
+
 #include "Eigen/Core"
 
 using namespace std;
 
-
-// Trains a logistic classifier
-
 namespace ml {
 
-// A logistic classifier.
 class LogisticClassifier {
 public:
-  LogisticClassifier() {
+  LogisticClassifier(const double& threshold_true) : threshold_true_(threshold_true) {
+    if (threshold_true < 0 || threshold_true > 1) {
+      throw "bad threshold";
+    }
   }
 
-  void fit(const Eigen::MatrixXd X, const std::vector<bool>& y) {
-
+  // Fit the classifier to data.
+  void fit(const std::vector<double>& x, const std::vector<bool>& y) {
+    x_ = x;
+    y_ = y;
   }
 
-  const bool predict(const Eigen::MatrixXd x) {
+  // Predict whether the input is true/false given the training data.
+  const bool predict(const double x) {
+    if (x_.size() == 0) {
+      throw "no data";
+    }
+    if (x > threshold_true_) {
+      return true;
+    }
+    return false;
   }
 private:
-
+  std::vector<double> x_;
+  std::vector<bool> y_;
+  const double threshold_true_;
 };
 
 
 }; // namespace ml
 
-// To execute C++, please define "int main()"
 int main() {
   auto words = { "Hello, ", "World!", "\n" };
   for (const string& word : words) {
