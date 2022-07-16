904d650924946cd5486cd6027aab568da60a9b95
unknown <kujayames@gmail.com>
2022-07-15 20:00:43 -0700

    Reduce runtime from 201 ms to 12 ms but memory from 10.2 MB to 12.1 MB

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen;
+        for (int i = 0; i < nums.size(); ++i) {
+            seen[nums[i]] = i;
+        }
         for (int i = 0; i < nums.size(); ++i) {
             const int new_target = target - nums[i];
-            for (int j = i+1; j < nums.size(); ++j) {
-                if (nums[j] == new_target) {
-                    return {i, j};
-                }
+            if (seen.count(new_target) == 1 && i != seen[new_target]) {
+                return {i, seen[new_target]};
             }
         }
         return {};
